b990107334682b44357502cc84d8e45d694a5ec8
toma2023 <mrinmoyehimadrita@gmail.com>
2023-03-27 01:54:27 +0600

    product cart added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/shop">shop</a>
-                <a href="/order">order</a>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
                 <a href="/inventory">Inventory</a>
                 <a href="/Login">Login</a>
             </div>
