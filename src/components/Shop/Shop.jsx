bb7a23a9cb9f4fdb65c68a0887da239a03b865f0
toma2023 <mrinmoyehimadrita@gmail.com>
2023-03-28 02:42:56 +0600

    show data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -36,7 +36,20 @@ setCart(savedCart);
 
     const handleAddToCart = (product) => {
         // cart.push(product);
-        const newCart = [...cart, product];
+        let newCart = [];
+       // const newCart = [...cart, product];
+        //if product doesn't exist in the cart then set quantity = 1;
+        //if exist update the quantity by 1
+const exists = cart.find(pd=> pd.id === product.id);
+if(!exists){
+    product.quantity= 1;
+    newCart = [...cart, product]
+}
+else{
+   exists.quantity = exists.quantity + 1;
+   const remaining = cart.filter(pd =>pd.id !== product.id);
+   newCart = [...remaining, exists];
+}
         setCart(newCart);
         addToDb(product.id);
     }
